eae3b8e2e3eb558a16cde051a9750da96545b6c7
minche0l <100738446+minche0l@users.noreply.github.com>
2022-05-04 09:50:12 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ import setuptools
 setuptools.setup (
   include_package_data = True,
   name='mycalc'
-  version='0.0.1',
-  description='oss-dev my calc'
-  author='mincheolkim',
-  author-email='kio7543@naver.com',
+  version ='0.0.1',
+  description ='oss-dev my calc'
+  author ='mincheolkim',
+  author-email ='kio7543@naver.com',
   url = "https://github.com/minche0l/mycalc/new/main",
+  downloadurl = "https://github.com/minche0l/mycalc/releases/tag/v1.0.0.zip",
   install_requires=['pytest'],
   long_description = 'oss-dev calculator python module',
   long_description_content_type='text/markdown',
